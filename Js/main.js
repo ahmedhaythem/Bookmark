46ed76b7920b31d40f2bf97b76587daf90a8e287
ahmedhaythem <ahmedhaythem240@gmail.com>
2024-12-13 14:33:10 +0200

    validation add

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,10 @@
 var siteName=document.getElementById('siteName');
 var websiteURL=document.getElementById('websiteURL');
 var btnSubmit=document.querySelector('.btn-submit');
-var namereg=/^\w{3,}(\s+\w+)*$/;
+var btnClose=document.querySelector('#closeBtn');
+var waringBox=document.getElementById('waring-box');
+var nameRegex=/^\w{3,}(\s+\w+)*$/;
+var siteRegex=/^\w{3,}\.\w{2,}/;
 var siteContainer;
 
 if(localStorage.getItem('sites')!=null){
@@ -25,8 +28,15 @@ function addSite(){
 }
 
 btnSubmit.addEventListener('click',function(){
-    addSite();
+    if(siteName.classList.contains('is-valid') && websiteURL.classList.contains('is-valid')){
+        addSite();
+    }
+    else{
+        waringBox.classList.replace('d-none','d-flex')
+    }
 })
+
+
 
 
 function clearForm(){
@@ -42,7 +52,7 @@ function displaySite(arr){
                         <td>${i+1}</td>
                         <td class="text-capitalize">${arr[i].siteName}</td>
                         <td><button onclick='visitSite(${i})' class="btn btn-visit"><i class="fa-solid fa-eye pe-2"></i>Visit</button></td>
-                        <td><button onclick='deleteSIte(${i})' class="btn btn-delete"><i class="fa-solid fa-trash-can pe-2"></i>Delete</button></td>
+                        <td><button onclick='deleteSite(${i})' class="btn btn-delete"><i class="fa-solid fa-trash-can pe-2"></i>Delete</button></td>
                     </tr>`
     }
     document.getElementById('tableBody').innerHTML=cartona
@@ -54,8 +64,35 @@ function visitSite(index){
     window.open(visitURL,'_blank')
 }
 
-function deleteSIte(index){
+function deleteSite(index){
     siteContainer.splice(index,1)
     displaySite(siteContainer)
     localStorage.setItem('sites',JSON.stringify(siteContainer))
 }
+
+function validInput(input,regex){
+    if(regex.test(input.value)){
+        input.classList.add('is-valid')
+        input.classList.remove('is-invalid')
+    }
+    else{
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+    }
+
+}
+
+
+btnClose.addEventListener('click',function(){
+    waringBox.classList.replace('d-flex','d-none')
+})
+
+siteName.addEventListener('input',function(){
+    validInput(siteName,nameRegex)
+
+})
+
+websiteURL.addEventListener('input',function(){
+    validInput(websiteURL,siteRegex)
+})
+
